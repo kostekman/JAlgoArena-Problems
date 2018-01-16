b973f96a7bc24a3dfc2a3d7424c2990a8752cffa
Adam Koscinski <kostekman1@gmail.com>
2018-01-16 15:57:42 +0100

    Adding initial swagger integration.

--- src/main/kotlin/com/jalgoarena/JAlgoArenaProblemsApp.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaProblemsApp.kt
@@ -6,9 +6,15 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 @Configuration
 open class JAlgoArenaProblemsApp {
     @Bean
@@ -18,6 +24,15 @@ open class JAlgoArenaProblemsApp {
         @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(JAlgoArenaProblemsApp::class.java, *args)
         }
+    }
+
+    @Bean
+    open fun problemsApi(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
     }
 }
 
